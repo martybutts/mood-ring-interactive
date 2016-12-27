1707649ce997f17ad03e8a82a22c4a490c926351
Martin <martinbutts@clear.net.nz>
2016-12-27 18:11:04 +1300

    add api keys

--- routes/index.js
+++ routes/index.js
@@ -7,3 +7,9 @@ var sentiment = require('sentiment')
 dotenv.load()
 
 //configure twitter client
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+})
