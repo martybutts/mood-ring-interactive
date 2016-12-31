0ef24a79ff17f753e7848202689ddb44a9c3c112
Martin <martinbutts@clear.net.nz>
2016-12-31 21:27:12 +1300

    set up client side

---
+++ public/client/client.js
@@ -0,0 +1,18 @@
+var sockett = io()
+let div = document.querySelector('.row>div')
+
+socket.on('connect', function() {
+  console.log("socket on")
+})
+
+socket.on('color', function(data) {
+  console.log('data', data)
+
+  var { rgb } = data
+
+  div.style.backgroundColor = 'rgb(' + [rgb[0], rgb[1], rgb[2]].join(',') + ')'
+  if (div.nextElementSibling)
+    div = div.nextElementSibling
+  else
+    div = div.parentElement.children[0]
+})
