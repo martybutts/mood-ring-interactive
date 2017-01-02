c2e2e8c3047c6a3dcfa1000167a25774f7c4a928
Martin <martinbutts@clear.net.nz>
2017-01-02 20:48:29 +1300

    add socket.io code to index.js

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,47 @@ var dotenv = require('dotenv')
 var Twitter = require('twitter')
 var sentiment = require('sentiment')
 
-//load environment variables
 dotenv.load()
 
-//configure twitter client
 var client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 })
+
+var sentimentToRed = function (score) {
+  return Math.round( (12.8 * score) + 127.8)
+}
+var sentimentToBlue = function (score) {
+  return  Math.round (255 - ((12.8 * score) + 127.8) )
+}
+
+var extractScore = function (event) {
+  var tweet = event && event.text;
+  var result = sentiment(tweet);
+  var score = result.score;
+console.log(tweet)
+console.log('')
+  return score
+}
+
+module.exports = function (io) {
+
+  io.on('connection', function (socket) {
+    var stream = client.stream('statuses/filter', {language: 'en', track: 'trump'});
+
+    stream.on('data', function(event) {
+      var score = extractScore(event)
+
+      var rgb = [sentimentToRed(score), 0, sentimentToBlue(score)]
+
+      socket.emit('color', { rgb: rgb });
+
+    })
+
+    stream.on('error', function(error) {
+      throw error;
+    });
+  });
+}
